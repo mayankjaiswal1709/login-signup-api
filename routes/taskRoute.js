3b11c6c3cedc07af7e3eae21f98c22aea9fd4675
Mayank Jaiswal <mayankjaiswal20180@gmail.com>
2023-09-14 15:30:04 +0530

    task assigned to project now project id pass in params

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,12 +1,12 @@
 const task = require('../controllers/taskControllers')
 const express = require('express')
-const  upload  = require('../middleware/multer')
+// const  upload  = require('../middleware/multer')
 const router = express.Router()
 const { isUser, isAdmin, isClient } = require('../middleware/authorization')
 
-router.post('/addtask/:pid/:userRole?',isAdmin, upload.single('task_image'), task.addTask)
+router.post('/addtask/:pid/:userRole?',isAdmin, task.addTask)
 router.get('/getalltask', task.getTasks)
-router.get('/getassignedtasktoprject', task.getTaskAssignedProjects)
+router.get('/getassignedtasktoprject/:projectId', task.getTaskAssignedProjects)
 router.get('/gettaskstatus', task.getTaskByStatus)
 router.patch('/updatetaskstatus/:_id', isAdmin, task.updateTaskStatus);
 router.patch('/updatetask/:_id/:userRole?', isAdmin, task.editTask)
